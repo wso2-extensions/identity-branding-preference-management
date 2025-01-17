86872df9bed828067986e36d0ec511825af42906
sahandilshan <sahandilshan222@gmail.com>
2025-01-17 09:44:06 +0530

    Use common ai component

--- components/org.wso2.carbon.identity.branding.preference.management.core/src/main/java/org/wso2/carbon/identity/branding/preference/management/core/ai/BrandingAIPreferenceManager.java
+++ components/org.wso2.carbon.identity.branding.preference.management.core/src/main/java/org/wso2/carbon/identity/branding/preference/management/core/ai/BrandingAIPreferenceManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -18,19 +18,42 @@
 
 package org.wso2.carbon.identity.branding.preference.management.core.ai;
 
-import org.wso2.carbon.identity.branding.preference.management.core.exception.BrandingAIClientException;
-import org.wso2.carbon.identity.branding.preference.management.core.exception.BrandingAIServerException;
+import org.wso2.carbon.identity.ai.service.mgt.exceptions.AIClientException;
+import org.wso2.carbon.identity.ai.service.mgt.exceptions.AIServerException;
 
 /**
  * Interface for AI branding preference manager.
  */
 public interface BrandingAIPreferenceManager {
 
-    String generateBrandingPreference(String websiteURL) throws BrandingAIServerException, BrandingAIClientException;
+    /**
+     * This method will connect to the BrandingAI microservice and generate the branding preference for the given
+     * website URL.
+     *
+     * @param websiteURL Website URL for which the branding preference should be generated.
+     * @return Operation ID of the branding preference generation.
+     * @throws AIServerException When errors occurs from the AI service.
+     * @throws AIClientException When invalid request is sent to the AI service.
+     */
+    String generateBrandingPreference(String websiteURL) throws AIClientException, AIServerException;
 
-    Object getBrandingPreferenceGenerationStatus(String operationId) throws BrandingAIServerException,
-            BrandingAIClientException;
+    /**
+     * This method will connect to the BrandingAI microservice and get the status of the branding preference generation.
+     *
+     * @param operationId Operation ID of the branding preference generation.
+     * @return Status of the branding preference generation.
+     * @throws AIServerException When errors occurs from the AI service.
+     * @throws AIClientException When invalid request is sent to the AI service.
+     */
+    Object getBrandingPreferenceGenerationStatus(String operationId) throws AIClientException, AIServerException;
 
-    Object getBrandingPreferenceGenerationResult(String operationId) throws BrandingAIServerException,
-            BrandingAIClientException;
+    /**
+     * This method will connect to the BrandingAI microservice and get the result of the branding preference generation.
+     *
+     * @param operationId Operation ID of the branding preference generation.
+     * @return Result of the branding preference generation.
+     * @throws AIServerException When errors occurs from the AI service.
+     * @throws AIClientException When invalid request is sent to the AI service.
+     */
+    Object getBrandingPreferenceGenerationResult(String operationId) throws AIClientException, AIServerException;
 }
